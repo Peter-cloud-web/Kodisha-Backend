fd38ae8db47ad4c7b5620684911349ea0522ce63
Peter-cloud-web <petergichia165@gmail.com>
2025-01-20 11:51:35 +0300

    Create user service to encode and decode passwords

---
+++ demo/src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    public User register(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+}
