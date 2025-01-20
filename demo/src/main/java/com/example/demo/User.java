a6f0ffec9578c7ffaac136b4f37950bff7c90614
Peter-cloud-web <petergichia165@gmail.com>
2025-01-20 11:20:32 +0300

    Add user entity class and user repository

---
+++ demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String location;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+}
