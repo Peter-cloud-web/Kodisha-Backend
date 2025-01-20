74299365a6704fa43d0141ab3d9b33b2709affb3
Peter-cloud-web <petergichia165@gmail.com>
2025-01-20 12:10:28 +0300

    Create user controller class

---
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user){
+        User registeredUser =  userService.register(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username){
+        User user = userService.findByUsername(username);
+        return ResponseEntity.ok(user);
+    }
+}
