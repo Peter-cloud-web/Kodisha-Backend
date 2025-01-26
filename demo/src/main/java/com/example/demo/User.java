823a89b97fc21d08e720e518409437d8f62e1835
Peter-cloud-web <petergichia165@gmail.com>
2025-01-26 14:59:15 +0300

    remove database constraints for testing purposes

--- demo/src/main/java/com/example/demo/User.java
+++ demo/src/main/java/com/example/demo/User.java
@@ -1,25 +1,29 @@
 package com.example.demo;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import jakarta.validation.constraints.Email;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false,length = 512)
     private String username;
 
     @Column(nullable = false)
+    @NotBlank(message = "Password is required")
     private String password;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false,length = 512)
+    @Email(message = "Email should be valid")
     private String email;
 
     private String location;
--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@Validated
 public class UserController {
 
     @Autowired
